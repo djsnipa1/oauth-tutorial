0c82d42737c0791892232114c186989f3e5e32b1
Chad Boyce <chadboyce@gmail.com>
2022-06-05 07:44:37 +0000

    spotify login is working so far!

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,31 @@
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const SpotifyStrategy = require('passport-spotify').Strategy;
 const keys = require('./keys')
 
 passport.use(
-  new GoogleStrategy({
-  // options for the google strategy
-    callbackURL: '/auth/google/redirect',
-    clientID: keys.google.clientID,
-    clientSecret: keys.google.clientSecret
-  }), () => {
-  // passport callback function
+  new SpotifyStrategy({
+  // options for the spotify strategy
+    clientID: keys.spotify.clientID,
+    clientSecret: keys.spotify.clientSecret,
+    callbackURL: 'https://oauth-tutorial.djsnipa1.repl.co/auth/spotify/redirect'
+  }, (accessToken, refreshToken, expires_in, profile, done) => {
+      console.log(profile)
+        // return done(err, user);
   })
-)
+);
+
+
+// passport.use(
+//   new SpotifyStrategy(
+//     {
+//       clientID: client_id,
+//       clientSecret: client_secret,
+//       callbackURL: 'http://localhost:8888/auth/spotify/callback',
+//     },
+//     function (accessToken, refreshToken, expires_in, profile, done) {
+//       User.findOrCreate({spotifyId: profile.id}, function (err, user) {
+//         return done(err, user);
+//       });
+//     }
+//   )
+// );
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const passport = require('passport');
 
 // auth login
 router.get('/login', (req, res) => {
@@ -11,11 +12,23 @@ router.get('/logout', (req, res) => {
   res.send('logging out');
 })
 
+// auth with spotify
+router.get('/spotify', passport.authenticate('spotify', {
+   scope: ['user-read-email', 'user-read-private'], 
+  })
+);
 
-// auth with google
-router.get('/google', (req, res) => {
-  // handle with passport
-  res.send('logging in with google');
-});
+// router.get('/spotify/redirect', passport.authenticate('spotify'), (req, res) => {
+//   res.send('you reached the callback');
+// });
 
 module.exports = router;
+
+router.get(
+  '/spotify/redirect',
+  passport.authenticate('spotify', {failureRedirect: '/login'}),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
+);
